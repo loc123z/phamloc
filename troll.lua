c3e63bd0ab888b16be658a9ef466f14c8c13dcaa
loc123z <tranvulinhkind@gmail.com>
2025-07-25 21:00:58 +0700

    Add files via upload

---
+++ troll.lua
@@ -0,0 +1,6 @@
+local sound = Instance.new("Sound")
+sound.SoundId = "rbxassetid://99363922918872"
+sound.Volume = 1
+sound.Looped = false
+sound.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+sound:Play()
